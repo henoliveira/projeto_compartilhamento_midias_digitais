2bf2db747f62213cae573d45dd78c112415fa218
Henrique <holiveira@wattio.com.br>
2023-06-23 09:58:22 -0300

    asd

--- test-p2p/p2p.py
+++ test-p2p/p2p.py
@@ -1,4 +1,16 @@
 from pythonp2p import Node
 
-node = Node()
+
+class MyNode(Node):
+    def on_message(message, sender, private):
+        # Gets called everytime there is a new message
+        print(message)
+
+
+node = MyNode()
 node.start()
+
+# node.connect_to("44.211.213.91")
+# node.savestate()
+# node.loadstate()
+# node.send_message({"msg": "Oi do Xonha"}, "44.211.213.91")
