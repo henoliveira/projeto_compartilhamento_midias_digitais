70b6d3aca9000ef930e477024c771332cc274df7
Henrique <holiveira@wattio.com.br>
2023-06-23 11:48:27 -0300

    sadf

---
+++ test-p2p/aws.py
@@ -0,0 +1,16 @@
+from pythonp2p import Node
+
+
+class MyNode(Node):
+    def on_message(self, message, sender, private):
+        # Gets called everytime there is a new message
+        print(message)
+
+
+if __name__ == "__main__":
+    node = Node()
+    node.start()
+
+    node.loadstate()
+    node.connect_to("179.108.22.7")
+    node.savestate()
