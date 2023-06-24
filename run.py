e7e82361f123039378497cf3fcbef8cf469196a6
Henrique <holiveira@wattio.com.br>
2023-06-24 09:02:56 -0300

    asd

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     project_dir = os.path.dirname(os.path.realpath(__file__))
     app_dir = os.path.join(project_dir, "app")
     os.system(
-        f"cd {app_dir} && uvicorn main:app --host=localhost --port=1337 --workers=4"
+        f"cd {app_dir} && uvicorn main:app --host=0.0.0.0 --port=1337 --workers=4"
     )
