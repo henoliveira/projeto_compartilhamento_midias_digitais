c4da8cb695179defd11e8f89c1f865e48762e96f
Henrique <holiveira@wattio.com.br>
2023-06-24 08:53:33 -0300

    run

---
+++ run.py
@@ -0,0 +1,6 @@
+import os
+
+if __name__ == "__main__":
+    project_dir = os.path.dirname(os.path.realpath(__file__))
+    app_dir = os.path.join(project_dir, "app")
+    os.system(f"cd {app_dir} && uvicorn main:app --reload")
